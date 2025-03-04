65ad72f86bb1f4cd7edd8b33d5f5258449d8563b
unknown <promiseokpoto5050@gmail.com>
2025-03-04 15:34:15 +0100

    done with responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
         <div className="flex flex-col h-auto gap-1 large:w-40 small:w-100">
           <div>Subscribe To our newsletter</div>
           <div className="flex items-center h-auto w-100">
-            <input type="text" placeholder="Email Address" className="p-1 rounded-tl rounded-bl h-40px w-90"/>
+            <input type="text" placeholder="Email Address" className="p-1 text-black rounded-tl rounded-bl h-40px w-90"/>
             <button className="flex items-center justify-center w-auto px-1 rounded-tr rounded-br h-40px bg-crossLightPurple">Subscribe</button>
           </div>
         </div>
