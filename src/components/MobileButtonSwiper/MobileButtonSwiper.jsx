4ef67193262e7c548c524f3900d251a4cc216b58
unknown <promiseokpoto5050@gmail.com>
2025-03-06 14:53:04 +0100

    added buttons

--- src/components/MobileButtonSwiper/MobileButtonSwiper.jsx
+++ src/components/MobileButtonSwiper/MobileButtonSwiper.jsx
@@ -1,62 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './MobileButtonSwiper.css';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./MobileButtonSwiper.css";
+import { MdChevronRight } from "react-icons/md";
+import { RiArrowLeftSLine } from "react-icons/ri";
 
 
 
 function MobileButtonSwiper() {
-    const routes = [
-        {
-        name: "Overview",
-        route: "about-us"
-      },
-      {
-        name: "Our story",
-        route: "our-story"
-      },
-      {
-        name: "Leadership",
-        route: "leadership"
-      },
-      {
-        name: "Our clients",
-        route: "our-clients"
-      },
-      {
-        name: "Participant’s Feedback",
-        route: "feedbacks"
-      },
-      {
-        name: "Satisfaction Guaranteed",
-        route: "satisfaction"
-      },
-      {
-        name: "Crosstie photos",
-        route: "crosstie-photos"
-      }
-    
-    ]
-    
-      return (
-        <div className="border mobile-swiper-container">
-          {/* <button className="nav-btn left" onClick={scrollLeft}>&#10094;</button> */}
-          
-          <div className="mobile-buttons-wrapper" 
+  const routes = [
+    {
+      name: "Overview",
+      route: "about-us",
+    },
+    {
+      name: "Our story",
+      route: "our-story",
+    },
+    {
+      name: "Leadership",
+      route: "leadership",
+    },
+    {
+      name: "Our clients",
+      route: "our-clients",
+    },
+    {
+      name: "Participant’s Feedback",
+      route: "feedbacks",
+    },
+    {
+      name: "Satisfaction Guaranteed",
+      route: "satisfaction",
+    },
+    {
+      name: "Crosstie photos",
+      route: "crosstie-photos",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col items-center h-auto gap-1 w-90vw">
+      <div className="border mobile-swiper-container">
+        {/* <button className="nav-btn left" onClick={scrollLeft}>&#10094;</button> */}
+
+        <div
+          className="mobile-buttons-wrapper"
           // ref={scrollContainerRef}
-          >
-            {routes.map((item, index) => (
-              <Link to={`/${item.route}`} key={index} className="flex items-center justify-center w-auto px-1 mobile-swiper-button text-10px rounded-20 h-30px bg-crossLightPurple">{item.name}</Link>
-            ))}
-          </div>
-    
-          {/* <button className="nav-btn right" onClick={scrollRight}>&#10095;</button> */}
+        >
+          {routes.map((item, index) => (
+            <NavLink
+              to={`/${item.route}`}
+              key={index}
+              className="flex items-center justify-center w-auto px-1 font-semibold mobile-swiper-button text-10px rounded-20 h-30px bg-crossLightPurple"
+            >
+              {item.name}
+            </NavLink>
+          ))}
         </div>
-      );
+
+        
+      </div>
+
+        <div className="flex items-center justify-center h-auto gap-2 text-black w-100">
+            <RiArrowLeftSLine className="text-gray-600 text-30px"/>
+            <MdChevronRight className="text-gray-600 text-30px"/>
+        </div>
+      {/* <button className="nav-btn right">this is</button> */}
+    </div>
+  );
 }
 
 export default MobileButtonSwiper;
-
-
-
-
-
