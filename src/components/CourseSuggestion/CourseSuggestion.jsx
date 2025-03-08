11a69676cbf54eb0e50704f06b055682bbbabc67
unknown <promiseokpoto5050@gmail.com>
2025-03-08 06:36:56 +0100

    working on about page

--- src/components/CourseSuggestion/CourseSuggestion.jsx
+++ src/components/CourseSuggestion/CourseSuggestion.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { courseData } from '../../assets/data';
 import { Link } from 'react-router-dom';
+import { CrossContext } from '../../Context/CrossContext';
 
 
 
@@ -16,12 +17,18 @@ function CourseSuggestion() {
   )
 }
 
+
 function MobileCourseSuggestion() {
+
+    const {toggleMobileSearch} = useContext(CrossContext);
+    
     return (
       <div className='flex flex-col items-start p-2 overflow-y-scroll bg-white w-80vw h-250px text-15px'>
         {
           courseData && courseData.map((course, i)=>
-          <Link key={i} className='p-1 border-b w-100'>{course.title}</Link>
+          <Link to="/about-us" key={i} className='p-1 border-b w-100'
+          onClick={toggleMobileSearch}
+          >{course.title}</Link>
           )
         }
       </div>
