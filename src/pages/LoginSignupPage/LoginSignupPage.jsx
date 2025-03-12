886ac0b507c4f377441797dd7b6956652e7a68cd
unknown <promiseokpoto5050@gmail.com>
2025-03-12 04:57:38 +0100

    working on enrollment

--- src/pages/LoginSignupPage/LoginSignupPage.jsx
+++ src/pages/LoginSignupPage/LoginSignupPage.jsx
@@ -8,14 +8,15 @@ function LoginSignupPage() {
   const [activeForm, setActiveForm] = useState("login");
 
   return (
-    <div className="flex flex-col items-center w-80vw mt-17 large:text-15px">
+    <div className="flex flex-col items-center large:w-80vw mt-17 text-15px small:w-90vw">
+      
       <div className="flex items-center justify-start h-auto w-100">
         <button
           className={`flex items-center justify-center ${
             activeForm === "login"
               ? "bg-white text-crossLightPurple underline"
               : ""
-          } rounded-tl-10 rounded-tr-10 h-100px w-200px`}
+          } rounded-tl-10 rounded-tr-10 large:h-100px large:w-200px small:h-50px small:w-100px`}
           onClick={() => setActiveForm("login")}
         >
           Login
@@ -25,24 +26,26 @@ function LoginSignupPage() {
             activeForm === "signup"
               ? "bg-white text-crossLightPurple underline"
               : ""
-          } rounded-tl-10 rounded-tr-10 h-100px w-200px`}
+          } rounded-tl-10 rounded-tr-10 h-100px w-200px large:h-100px large:w-200px small:h-50px small:w-100px`}
           onClick={() => setActiveForm("signup")}
         >
           Sign up
         </button>
       </div>
 
+
+
       {/* sign up */}
       {activeForm === "signup" && (
         <div className="flex flex-col items-center gap-3 py-3 bg-white rounded-10 w-100">
           <div className="flex flex-col items-center w-auto h-auto">
-            <h4 className="font-semibold text-20px">Create New Account</h4>
+            <h4 className="font-semibold large:text-20px">Create New Account</h4>
             <p className="text-crossTextGray">
               (If you are not a Crosstie student)
             </p>
           </div>
 
-          <div className="flex flex-col items-center gap-2 w-60">
+          <div className="flex flex-col items-center gap-2 large:px-0 small:px-1 large:w-60 small:w-100">
             <div className="flex flex-col h-auto w-100">
               <label htmlFor="">First Name*</label>
 
@@ -84,17 +87,21 @@ function LoginSignupPage() {
       )}
 
       {/* login */}
+      
       {activeForm === "login" && (
         <div className="flex flex-col items-center gap-3 py-3 bg-white rounded-10 w-100">
           <div className="flex flex-col items-center w-auto h-auto">
-            <h4 className="font-semibold text-20px">Login to Your Account</h4>
+            <h4 className="font-semibold large:text-20px">Login to Your Account</h4>
           </div>
 
-          <div className="flex flex-col items-center gap-2 w-60">
+          <div className="flex flex-col items-center gap-2 large:px-0 small:px-1 large:w-60 small:w-100">
+            
+
             <div className="flex flex-col h-auto w-100">
               <label htmlFor="">Email*</label>
 
               <input type="email" className="p-1 border rounded h-40px w-100" />
+
               <p className="text-crossTextGray text-10px">
                 If you are already registered for the class by your employer,
                 use your official Email ID to login.
