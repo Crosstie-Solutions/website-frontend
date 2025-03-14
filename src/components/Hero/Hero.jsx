b027cfdb68d02d7dfc5dc90dc415de244fbbd8cb
unknown <promiseokpoto5050@gmail.com>
2025-03-14 21:05:12 +0100

    done with user profile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ function Hero() {
           Discover new skills and interests with our extensive course collection
         </p>
 
-        <Link className="flex items-center justify-center w-auto gap-1 px-1 mt-2 font-bold rounded-10 h-40px bg-buttonOverlay text-15px">
+        <Link to='/about-us/' className="flex items-center justify-center w-auto gap-1 px-1 mt-2 font-bold rounded-10 h-40px bg-buttonOverlay text-15px">
           More about us <BsArrowRight />
         </Link>
 
